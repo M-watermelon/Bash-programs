da64d7921fecd088620f2b63c064e74853a1d312
Maria M <87876938+M-watermelon@users.noreply.github.com>
2022-08-06 22:46:06 -0500

    Add files via upload

---
+++ BASH projects/notes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# this is a comment
+# second comment
+
+echo "Hello World" #print out hello world
+cat > file.txt #replace text of file.txt
+cat >> file.txt #append new text ahead of og text in file.txt
+
+
+
